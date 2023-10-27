4ebeb4ab72ef3b5c2accf7c300028aee96fe1457
Trần Nguyên Đức <tranxuansuc12345@gmail.com>
2023-10-27 21:39:52 +0700

    update amount + update rates

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const rubToUsdRate = 0.010736;
-const usdToVndRate = 24575;
+const rubToUsdRate = 0.0107182;
+const usdToVndRate = 24580;
 const pieChartCanvas = document.getElementById('asset-pie-chart');
 let usdAmountValue = parseFloat(document.getElementById('usd-amount').textContent.replace('$', ''));
 let rubAmountValue = parseFloat(document.getElementById('rub-amount').textContent.split(' ')[1]);
